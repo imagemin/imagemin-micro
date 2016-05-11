0936a21a6eeee3d4dbe181d48d80ed1c998a32d6
Kevin Martensson <kevinmartensson@gmail.com>
2016-05-11 17:35:11 +0200

    Improve tests

--- test.js
+++ test.js
@@ -5,20 +5,28 @@ import nock from 'nock';
 import test from 'ava';
 import m from './';
 
-test(async t => {
-	const app = m();
-	const scope = nock('http://foo.com')
+let app;
+let scope;
+
+test.before(() => {
+	app = m();
+	scope = nock('http://foo.com')
 		.get('/test.png')
 		.replyWithFile(200, path.join(__dirname, 'fixture.png'));
 
-	app.listen('3000');
+	app.listen(3000);
+});
 
+test.after(t => {
+	t.true(scope.isDone());
+});
+
+test(async t => {
 	const res = await got.post('http://localhost:3000/image', {
 		body: JSON.stringify({url: 'http://foo.com/test.png'}),
 		encoding: null,
 		headers: {'content-type': 'application/json'}
 	});
 
-	t.true(scope.isDone());
 	t.true(isPng(res.body));
 });
